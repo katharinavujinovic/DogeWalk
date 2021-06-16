f9236984837b7f6d01c771d6508a38560a945b4d
Katharina Muellek <katharina.muellek@hotmail.com>
2021-06-16 16:28:51 +0200

    initial creation of StatsVC

---
+++ DogeWalk/Controller/StatsOverviewViewController.swift
@@ -0,0 +1,18 @@
+//
+//  StatsOverviewViewController.swift
+//  DogeWalk
+//
+//  Created by Katharina Müllek on 16.06.21.
+//
+
+import Foundation
+import UIKit
+
+class StatsOverviewViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
